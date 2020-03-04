573762e54c4525ca065cbed9458b44656bad312f
kushthakkar <kushthakkar08@gmail.com>
2020-03-04 22:37:15 +0530

    config change

--- angular/dist/angular/assets/config/configs.ts
+++ angular/dist/angular/assets/config/configs.ts
@@ -1,6 +1,6 @@
 export const config = {
   // "apiUrl": "http://localhost:4000/",
-  "apiUrl": "http://localhost:4000/",
+  "apiUrl": "http://swatch-007.com/",
   "assetUrl":"https://easocare.s3.ap-south-1.amazonaws.com/",
   "port": "4070"
 }
